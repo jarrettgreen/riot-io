8d209372fa01bff61f3959642766d4b20af4b111
Michael Spohn <mikeLspohn@gmail.com>
2016-10-10 18:00:57 -0500

    Make Home a pure component

--- client/components/home.js
+++ client/components/home.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 
-class Home extends Component {
-  render() {
-    return(
-      <h1> Welcome to Riot! </h1>
-    )
-  }
+const Home = () => {
+  return (
+    <h1> Welcome to Riot! </h1>
+  )
 }
 
 export default Home
