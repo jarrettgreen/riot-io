da1ed07c96c765161965a518e8d009364b310526
Jarrett Green <himself@jarrettgreen.com>
2016-12-06 18:31:24 -0600

    jsx-standard stuff

--- client/modules/Dashboard/pages/Dashboard.show.js
+++ client/modules/Dashboard/pages/Dashboard.show.js
@@ -1,44 +1,41 @@
-import React, {Component} from 'react'
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import NewWidget from '../../Widget/components/NewWidget'
 import WidgetList from '../../Widget/WidgetList'
 
-import { fetchDashboard } from '../DashboardActions';
-import { getDashboard } from '../DashboardReducer';
-
-
-
+import { fetchDashboard } from '../DashboardActions'
+import { getDashboard } from '../DashboardReducer'
 
 class DashboardView extends Component {
-  componentDidMount() {
-    this.props.dispatch(fetchDashboard(this.props.params.id));
+  componentDidMount () {
+    this.props.dispatch(fetchDashboard(this.props.params.id))
   }
 
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
   }
 
-  render() {
+  render () {
     if (this.props.dashboard){
       return(
 
         <div>
           <NewWidget/>
-          <h1 className="page-title">{this.props.dashboard.title}</h1>
+          <h1 className='page-title'>{this.props.dashboard.title}</h1>
 
-          <div className="row">
+          <div className='row'>
             <WidgetList widgets={this.props.dashboard.dashboardWidgets} />
           </div>
         </div>
       )
-    } else { return <div>loading...</div>}
+    } else { return <div>loading...</div> }
   }
 }
 
-function mapStateToProps(state, props) {
+function mapStateToProps (state, props) {
   return {
-    dashboard: getDashboard(state, props.params.id),
-  };
+    dashboard: getDashboard(state, props.params.id)
+  }
 }
 
-export default connect(mapStateToProps)(DashboardView);
+export default connect(mapStateToProps)(DashboardView)
