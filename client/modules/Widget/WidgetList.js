3bec730261e42b3a4c88673dfa055b51bfc04d60
Jarrett Green <jarrett@balancedcomp.com>
2017-01-30 15:50:18 -0600

    formatting

--- client/modules/Widget/WidgetList.js
+++ client/modules/Widget/WidgetList.js
@@ -1,22 +1,19 @@
-import React, { component } from 'react'
+import React from 'react'
 import SwitchWidget from './components/SwitchWidget'
 // import Widget from './Widget'
 
-function WidgetList(props) {
-
-
+function WidgetList (props) {
   return (
     <div>
       {
         props.widgets.map(widget => {
-          switch(widget.type){
+          switch (widget.type) {
             case 'SwitchWidget': return <SwitchWidget widget={widget} key={widget._id} />
-            break
           }
         })
       }
     </div>
-  );
+  )
 }
 
-export default WidgetList;
+export default WidgetList
--- client/modules/Widget/components/NewWidget.js
+++ client/modules/Widget/components/NewWidget.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { initWidget } from '../WidgetActions'
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 class NewWidget extends Component {
 
@@ -8,25 +8,25 @@ class NewWidget extends Component {
     super(props)
   }
 
-  handleClick = () => {
-    this.props.dispatch(initWidget('SwitchWidget'))
-  }
+  // handleClick = () => {
+  //   this.props.dispatch(initWidget('SwitchWidget'))
+  // }
 
   render () {
     return (
       <div className='dropdown'>
-        <button className='btn btn-default dropdown-toggle' type='button' id='dropdownMenu1' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>
+        <button className='btn btn-default dropdown-toggle' type='button' id='new-widget-dropdown' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>
           New Widget
-          <span className='caret'></span>
+          <span className='caret' />
         </button>
-        <ul className='dropdown-menu' aria-labelledby='dropdownMenu1'>
-          <li><a onClick={this.handleClick}>ToggleSwitch</a></li>
+        <ul className='dropdown-menu' aria-labelledby='new-widget-dropdown'>
+          <li><a> ToggleSwitch </a></li>
         </ul>
       </div>
     )
   }
 }
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps (dispatch) {
   return { actions: bindActionCreators(initWidget, dispatch) }
 }
 export default connect(mapDispatchToProps)(NewWidget)
